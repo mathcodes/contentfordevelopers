bfb7c45ce99b7d7e78cefe8377b33066974db8ad
Jon Christie <jonchristie@Jons-MBP.attlocal.net>
2022-07-31 08:22:52 -0600

    remove

--- lc_hr_cw/LeetCode/0027. Remove Element/removeElement.js
+++ lc_hr_cw/LeetCode/0027. Remove Element/removeElement.js
@@ -13,6 +13,17 @@ var removeElement = function(nums, val) {
   console.log(nums);
 };
 
+// OR
+
+const removeElement2 = (nums, val) => {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i--, 1)
+    }
+  }
+  return nums.length
+}
+
 
  
 
