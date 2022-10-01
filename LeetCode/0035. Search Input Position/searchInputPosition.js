ba2264e2e661a488637bf7b32523eb23e8136c83
Suman Sahoo <75962762+suman-somu@users.noreply.github.com>
2022-10-01 07:49:34 +0530

    Create searchInputPosition.js

---
+++ LeetCode/0035. Search Input Position/searchInputPosition.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if(nums.includes(target)){
+        return nums.indexOf(target);  
+    }
+    else{
+        nums.push(target);
+        nums.sort((a, b) => {return a-b})
+        return nums.indexOf(target);  
+    }
+};
