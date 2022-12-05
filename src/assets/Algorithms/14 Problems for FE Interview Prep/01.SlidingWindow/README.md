cfa9b71e81f559f269bd557a1437ece91b5b487e
mathcodes <jonpchristie@gmail.com>
2022-12-05 11:45:53 -0500

    14 frontend problems

---
+++ src/assets/Algorithms/14 Problems for FE Interview Prep/01.SlidingWindow/README.md
@@ -0,0 +1,14 @@
+# Sliding Window
+
+A pattern used when given an `array` or `linked list`, we need to perform a required operation on a specific window size. 
+
+<img src="./slidingwindow.png" />
+
+To identify the Sliding Window:
+- Input is a linear data structure (array, linked list, or a string)
+- Goal is to find the longest/shortest subarray, substring, or desired value
+
+Problem that can be solved using the Sliding Window:
+- Maximum sum subarray of size ‘K’ (E)
+- Longest substring with ‘K’ distinct characters (M)
+- String anagrams (H)
--- src/assets/LeetCode/0053. Maximum Subarray/maximumSubarray.js
+++ src/assets/LeetCode/0053. Maximum Subarray/maximumSubarray.js
@@ -1,13 +1,32 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-function maxSubArray(nums) {
-  let prev = -Infinity;
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function maxSubArray(nums) {
+//   let prev = -Infinity;
 
-  return nums.reduce((high, num) => {
-    prev = Math.max(num, prev + num);
-    return Math.max(high, prev);
-  }, nums[0]);
-};
+//   return nums.reduce((high, num) => {
+//     prev = Math.max(num, prev + num);
+//     return Math.max(high, prev);
+//   }, nums[0]);
+// };
 
+
+const maxSubArray = (nums) => {
+  let solution = nums[0]; // define my solution by default will be the first varaiable  
+
+  for (let i = 1; i < nums.length; i++) {
+    // we could create a cache array that stores our solution elements in order not to mutate the array(or the inputs)
+    // my numbers at i is going ot be the max of the two possibilities: nums[i] OR nums[i] + nums[i-1]
+    console.log("nums[i]: ", nums[i]);
+    nums[i] = Math.max(nums[i], nums[i] + nums[i-1]); 
+    console.log("nums[i]: ", nums[i]);
+    // and as we go through we can say that our solution is either the solution OR nums[i]
+    console.log("solution ", solution);
+    solution = Math.max(solution, nums[i]);
+    console.log("solution ", solution);
+  }
+  return solution;
+}
+
+maxSubArray([100, -2, 3, -3, 0, 9, -8, 2])
