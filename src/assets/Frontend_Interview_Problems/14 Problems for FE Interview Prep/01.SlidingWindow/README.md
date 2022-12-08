695fa6d1e1050f8313019ea343cf2437d90999f8
mathcodes <jonpchristie@gmail.com>
2022-12-08 12:33:28 -0500

    READMEs

--- src/assets/Frontend_Interview_Problems/14 Problems for FE Interview Prep/01.SlidingWindow/README.md
+++ src/assets/Frontend_Interview_Problems/14 Problems for FE Interview Prep/01.SlidingWindow/README.md
@@ -4,11 +4,59 @@ A pattern used when given an `array` or `linked list`, we need to perform a requ
 
 <img src="./slidingwindow.png" />
 
-To identify the Sliding Window:
+# To identify the Sliding Window:
 - Input is a linear data structure (array, linked list, or a string)
 - Goal is to find the longest/shortest subarray, substring, or desired value
 
-Problem that can be solved using the Sliding Window:
+# Problems that can be solved using the Sliding Window:
 - Maximum sum subarray of size ‘K’ (easy)
-- Longest substring with ‘K’ distinct characters (medium)
+
+<details>
+<summary>
+<span style="font-size:1.5rem;">Longest substring with ‘K’ distinct characters (medium)
+</span>
+</summary>
+
+
+```js
+var lengthOfLongestSubstringKDistinct = function(s, k) {
+  // SETUP VARIABLES
+  let START = 0;
+  const SOFAR = new Map();
+  let MAX = 0;
+
+  // ITERATE OVER s, our string, set END = 0
+  for (let END = 0; END < s.length; END++){
+    // pull out our rightCHAR
+    let rightCHAR = s[END];
+
+    // if our Map does not containt the rightCHAR, then we want to set it to 1
+    if(!SOFAR.get(rightCHAR)) SOFAR.set(rightCHAR, 1);
+    // if it does containt the rightCHAR, then we want ot increment it by 1
+    else SOFAR.set(rightCHAR, SOFAR.get(rightCHAR) + 1);
+
+    // NOW check the size (# of keys we have in SOFAR) and while it is > k
+    // THEN we have to decrement the START and move it to the right one
+    while(SOFAR.size > k) {
+      // so lets get our leftCHAR which = s[START]
+      let leftCHAR = s[START];
+
+      //SO IF > 1 leftCHAR we want to decrement (or DELETE) followed by increment our window START
+      if(SOFAR.get(leftCHAR) > 1) SOFAR.set(leftCHAR, SOFAR.get(leftCHAR) - 1);
+      else SOFAR.delete(leftCHAR);
+
+      START++;
+    }
+    // calculate MAX using Math.max
+    MAX = Math.max(MAX, (END - START) + 1)
+  }
+  // return MAX
+  return MAX;
+}
+
+```
+</details>
+
+
+
 - String anagrams (hard)
--- src/assets/Frontend_Interview_Problems/pg.js
+++ src/assets/Frontend_Interview_Problems/pg.js
@@ -1,33 +1,82 @@
-var firstMissingPositive = function(nums) {
-  let i = 0,
-  arrLength = nums.length;
+var lengthOfLongestSubstringKDistinct = function(s, k) {
+  let START = 0;
+  let stringMAP = new Map();
+  let MAX = 0;
 
-  while(i < arrLength){
-    const toSwap = nums[i],
-    swapWithIndex = toSwap - 1
+  for(let END = 0; END < s.length; END++){
+    let rightCHAR = s[END]
 
-    // we only want to SWAP if these three condition are met:
-    // toSwap > 0
-    //    our current value is positive
-    // toSwap <= arrLength
-    //    our current value is within the bounds of the array
-    // toSwap !== nums[swapWithIndex]
-    //    our current value does not equal the value of its destination
-    if(toSwap > 0 && toSwap <= arrLength && toSwap !== nums[swapWithIndex]){
-      const swapWith = nums[swapWithIndex]
-      nums[swapWithIndex] = toSwap
-      nums[i] = swapWith
-    } else {
-      i++
+    if(!stringMAP.get(rightCHAR)) stringMAP.set(rightCHAR, 1);
+    else stringMAP.get(rightCHAR, stringMAP.get(rightCHAR)+1);
+
+    while (stringMAP.size > k) {
+      let leftCHAR = s[START];
+
+      if(stringMAP.get(leftCHAR) > 1) stringMAP.set(leftCHAR, stringMAP.get(leftCHAR) -1);
+      else stringMAP.delete(leftCHAR);
+
+      START++;
     }
+    MAX = Math.max(MAX, (END - START) + 1)
   }
-  console.log(nums)
-  for (let j = 0; j < nums.length; j++){
-    if(nums[j] !== j+1) {
-      return j + 1
-    }
-  }
-  return arrLength + 1
+  return MAX
 }
 
-console.log(firstMissingPositive([0,2,4,-3,99,5,1]))
+
+var sortedSquares = function(nums) {
+  // SETUP VARIABLES 
+  // result array with same length, all zeros
+  const result = new Array(nums.length).fill(0);
+  let RIGHT = nums.length - 1;
+  let LEFT = 0;
+  let resultIndex = nums.length - 1;
+
+  // WHILE LOOP
+  // check while left is <= right we have to check which is bigger
+  // the set values for leftVal and rightVal = the squared values
+  while (LEFT <= RIGHT) {
+    let leftVal = Math.pow(nums[LEFT], 2);
+    let rightVal = Math.pow(nums[RIGHT], 2);
+    // IF ELSE STATEMENT 
+    // Then as long as leftVal < rightVal, the resultIndex = rightVal, 
+    // otherwise = leftVal
+    if (leftVal < rightVal) {
+      result[resultIndex] = rightVal;
+      RIGHT--;
+    } else {
+      result[resultIndex] = leftVal;
+      LEFT++;
+    }
+    // decremeent resultIndex and return
+    resultIndex--;
+  }
+  // RETURN 
+  return result;
+}
+
+var sortedSquares = function(nums) {
+    // SETUP VARIABLES 
+  // result array with same length, all zeros
+  const result = new Array(nums.length).fill(0);
+  let right = nums.length - 1;
+  let left = 0;
+  let resultIndex = nums.length - 1;
+
+  // WHILE LOOP
+  while(left <= right){
+    left leftValue = Math.pow(nums[left], 2)
+    left rightValue = Math.pow(nums[right], 2)
+
+    if (leftValue < rightValue){
+      result[resultIndex]
+    }
+  }
+  // check while left is <= right we have to check which is bigger
+  // the set values for leftVal and rightVal = the squared values
+    // IF ELSE STATEMENT 
+    // Then as long as leftVal < rightVal, the resultIndex = rightVal, 
+    // otherwise = leftVal
+
+    // decremeent resultIndex and return
+  // RETURN 
+}
