78c851e72f668e63a6c4f46fc927c8dd1368ce40
mathcodes <jonpchristie@gmail.com>
2022-12-23 16:49:03 -0500

    tried out react-modal. seems unstable. may resort to a reusable page

--- src/components/card/ItemCard.jsx
+++ src/components/card/ItemCard.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import * as React from 'react';
 import "./ItemCard.scss";
 import { leetCodeData } from '../../data/leetCode'
 import { styled } from '@mui/material/styles';
 // import Box from '@mui/material/Box';
-import {Paper, Grid, Card} from '@mui/material';
+import Editor from '../editor/Editor'
+import { Paper, Grid, Card } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import "../../pages/LeetCode.css"
+
+
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -12,6 +20,27 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
+
+const Item2 = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '80%',
+  height: '80%',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 let itemDifficulty = leetCodeData[0].difficulty
 console.log(itemDifficulty)
@@ -29,37 +58,102 @@ function cellColor(itemDifficulty) {
 }
 
 const ItemCard = () => {
-   
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
 
       {leetCodeData.map((item) => {
-       
+
         return (
-          <Card className="IndividualItemCard">
+          <Card onClick={handleOpen} className="IndividualItemCard">
+
             <Grid container spacing={2}>
-              
+
               <Grid item xs={12}>
                 <Item className="itemCardTitle">
                   <h2>{item.title}</h2>
                   <div className="tagWrapper">
-                  {item.tags.map((t) =>
-                  
-                  <div className="tag">{t}</div>)}
+                    {item.tags.map((t) =>
+
+                      <div className="tag">{t}</div>)}
                   </div>
                 </Item>
               </Grid>
-              <Grid item >
+              <Grid item xs={12}>
                 <div className="bottomItemCard">
-                <Item  className="itemCard2ndRow"><a href={item.link} target="blank">
-                  Original
-                </a></Item>
-                <Item  className="itemCard2ndRow" style={{ color: cellColor(itemDifficulty) }}>{item.difficulty}</Item>
-             </div>
+                  <Item className="itemCard2ndRow originalItem"><a href={item.link} target="blank">
+                    Original
+                  </a></Item>
+                  <Item
+                    className="itemCard2ndRow difficultyItem"
+                    style={{
+                      backgroundColor: item.difficulty === 'Easy'
+                        ?
+                        'green'
+                        :
+                        item.difficulty === 'Medium'
+                          ?
+                          'orange'
+                          :
+                          'red',
+                      color: item.difficulty === 'Easy'
+                        ?
+                        'limegreen'
+                        :
+                        item.difficulty === 'Medium'
+                          ?
+                          'yellow'
+                          :
+                          'black'
+                    }}
+                  >
+                    {item.difficulty}
+                  </Item>
+                </div>
               </Grid>
-     
+
             </Grid>
+            <Modal
+              style={{
+                margin: '3rem',
+              }}
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box
+                style={{
+                  padding: '1rem'
+                }}
+                sx={{ flexGrow: 2 }}>
+                <Grid container spacing={{ xs: 2}} columns={{ xs: 6, sm:6 }}>
+
+                  <Grid
+                    style={{
+                      backGround: ` #259fe9`
+                    }}
+                    item xs={2} sm={4} md={4}>
+                    <Item2><Editor /></Item2>
+
+                  </Grid>
+                  <Grid
+                    style={{
+                      backGround: ` #259fe9`
+                    }}
+                    item xs={2} sm={4} md={4}>
+                    <Item2>xs=2</Item2>
+
+                  </Grid>
+
+                </Grid>
+              </Box>
+            </Modal>
           </Card>
+
         );
       })}
     </>
--- src/pages/LeetCode.jsx
+++ src/pages/LeetCode.jsx
@@ -1,18 +1,38 @@
-import React from "react";
-// import Cards from '../components/Card'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 import { Contributor } from "../components/contributor/Contributor";
 import Footer from "../components/footer/Footer";
 import ItemCard from "../components/card/ItemCard";
 import NavBar from "../components/navbar/Navbar";
-
 import "./LeetCode.scss";
 
-function LeetCode() {
-	
 
+
+
+export default function Leetcode() {
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+
+	 const style = {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: 400,
+		bgcolor: 'background.paper',
+		border: '2px solid #000',
+		boxShadow: 24,
+		p: 4,
+	};
 	return (
 		<div className='leetCode'>
-			<NavBar/>
+			<NavBar />
+
+
 			<div className='banner'>
 				<h1>
 					LeetCode Problems
@@ -20,10 +40,27 @@ function LeetCode() {
 				</h1>
 				<h3>Choose the problem you'd like to explore!</h3>
 			</div>
-			
+			<div>
+			 
+				<Modal
+					open={open}
+					onClose={handleClose}
+					aria-labelledby="modal-modal-title"
+					aria-describedby="modal-modal-description"
+				>
+					<Box sx={style}>
+						<Typography id="modal-modal-title" variant="h6" component="h2">
+							Text in a modal
+						</Typography>
+						<Typography id="modal-modal-description" sx={{ mt: 2 }}>
+							Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+						</Typography>
+					</Box>
+				</Modal>
+			</div>
 			<div className="item_wrapper">
-			<ItemCard />
-				
+				<ItemCard onclick={handleOpen} />
+
 			</div>
 
 			<section className="contributor_section">
@@ -43,5 +80,3 @@ function LeetCode() {
 		</div>
 	);
 }
-
-export default LeetCode;
