947c6817e462524ae4946ff45c269116a7f4d0fc
jackz070 <95173354+jackz070@users.noreply.github.com>
2023-01-21 16:26:12 +0100

    feat-tailwind_refactor_contributor

--- src/components/homepage/contributorSection/ContributorSection.jsx
+++ src/components/homepage/contributorSection/ContributorSection.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { data } from '../../../data/db'
 import { Contributor } from '../../contributor/Contributor'
 
 //This is a component that appears on homepage to show contributors
 const ContributorSection = () => {
   return (
-  <section className="contributor_section-scrollbar mx-2 my-8 flex flex-col items-center">
+      <section className="contributor_section-scrollbar mx-2 my-8 flex flex-col items-center">
                 <h2 className="text-black mb-4">Contributors</h2>
                 <div className="flex flex-wrap justify-evenly p-6 overflow-y-scroll max-w-6xl max-h-[70vh] bg-blue-800 rounded-lg">
-                    <Contributor />
+                {data.map((res) => <Contributor res={res} key={res.id}/>)}
                 </div>
-    </section>
+        </section>
   )
 }
 
