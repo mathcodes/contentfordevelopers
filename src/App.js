edf005706cab4e23ee26f35c9a0ce7e8ba7d41ba
jackz070 <95173354+jackz070@users.noreply.github.com>
2023-01-26 22:14:58 +0100

    feat-header-rewrite-with-tailwind

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import Homepage from './pages/Homepage';
-import LeetCode from './pages/LeetCode';
-import ItemPage from './pages/ItemPage';
-// import Navbar from './components/navbar/Navbar';
-import Navbar from "./components/navbar/Navbar"
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Homepage from "./pages/Homepage";
+import LeetCode from "./pages/LeetCode";
+import ItemPage from "./pages/ItemPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NewNavbar from "./components/navbar/NewNavbar";
 
 function App() {
   return (
     <div className="max-w-full">
-      <Navbar/>
+      <NewNavbar />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage/>} />
-          <Route path="/leetcode" element={<LeetCode/>} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/leetcode" element={<LeetCode />} />
           <Route path="/leetcode/:id" element={<ItemPage />} />
-          <Route path="/itempage" element={<ItemPage/>} />
+          <Route path="/itempage" element={<ItemPage />} />
           {/* <Route path="/hackerrank" element={<HackerRank/>} />
           <Route path="/codewars" element={<CodeWars/>} />
           <Route path="/dsa" element={<DSA/>} />
           <Route path="/notesandresources" element={<NotesAndResources/>} /> */}
         </Routes>
-		  </BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
