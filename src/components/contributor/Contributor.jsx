d4d9640d0485089471c10bf9e61ab848a2ba2369
Jayaditya-Contributor <OpenSourceAditya@gmail.com>
2023-02-02 10:21:00 +0530

    added a Carousel

--- src/components/contributor/Contributor.jsx
+++ src/components/contributor/Contributor.jsx
@@ -5,7 +5,7 @@ export const Contributor = ({ res }) => {
     <div className="p-2 flex flex-col items-center" key={res.id}>
       <a href={res.html_url} target="blank">
         <img
-          className="rounded-full h-16 w-16 m-1 hover:scale-150 transition-all duration-300"
+          className="rounded-full md:h-24 md:w-24 h-16 w-16 m-1 hover:scale-150 transition-all duration-300"
           src={res.avatar_url}
           alt={res.login}
         />
