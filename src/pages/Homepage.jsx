732c3c65a8a341aee2d559f60462c38587fbb04c
mathcodes <jonpchristie@gmail.com>
2023-04-02 12:26:47 -0400

    data

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -14,7 +14,7 @@ function Homepage() {
       {/* <Header /> */}
       <NavBar />
       <Hero />
-      <TopicsGrid />
+      {/* <TopicsGrid /> */}
       <Learn />
       <Contribute />
       <Footer />
